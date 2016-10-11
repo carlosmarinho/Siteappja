583705ae38c9660aba30662c173e596f8fb40ee5
Carlos <carluizfla@hotmail.com>
2016-10-11 14:06:32 -0300

    adicionado sdk para exibir a fan page do facebook

--- header.php
+++ header.php
@@ -38,7 +38,14 @@ $theme_init =  prohost_theme_options();
 </head>
 
 <body <?php body_class(); ?>>
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=532309223460912";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 	<?php
 	prohost_profiler_add_point(esc_html__('BODY start', 'prohost'));
 	
